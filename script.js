fcf09f6520acc3ff788e7b0d29c9f37742b50b58
“haurylan” <haurilan25300@gmail.com>
2023-08-24 10:25:43 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarModo() {
+  const html = document.documentElement
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+}
+// html.classList.toggle('light')
